46fbb58f2e85966466286f81794ced74fa57560d
Tomas Martinez <martineztomi94@gmail.com>
2023-08-21 16:20:25 -0300

    Custom hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { ContadorRed } from "./components/ContadorRed";
 import { Counter } from "./components/Counter";
+import { Formulario } from "./components/Formulario";
 import { TimerPadre } from "./components/TimerPadre";
 import { Usuario } from "./components/Usuario";
 
@@ -20,6 +21,14 @@ function App() {
       <hr />
       <ContadorRed/>
 
+      <h2>CustomHooks</h2>
+      <hr />
+      <Formulario/>
+
+      <br />
+      <br />
+      <br />
+      <br />
     </>
   );
 }
---
+++ src/components/Formulario.tsx
@@ -0,0 +1,57 @@
+import { useForm } from '../hooks/useForm';
+
+interface FormData {
+    email:string,
+    nombre:string,
+    edad: number
+}
+
+export const Formulario = () => {
+
+    const { formulario, handleChange } = useForm<FormData>({
+        email: 'ejemplo@gmail.com',
+        nombre: 'Tomas Martinez',
+        edad: 35
+    })
+
+    const {email,nombre,edad} = formulario;
+
+  return (
+    <form autoComplete='off'>
+        <div className="mb-3">
+            <label className='form-label'>Email:</label>
+            <input 
+                type="email" 
+                className='form-control'
+                name='email'
+                value={ email }
+                onChange={handleChange}
+            />
+        </div>
+
+        <div className="mb-3">
+            <label className='form-label'>Nombre:</label>
+            <input 
+                type="text" 
+                className='form-control'
+                name='nombre'
+                value={ nombre }
+                onChange={handleChange}
+            />
+        </div>
+
+        <div className="mb-3">
+            <label className='form-label'>Edad:</label>
+            <input 
+                type="number" 
+                className='form-control'
+                name='edad'
+                value={ edad }
+                onChange={handleChange}
+            />
+        </div>
+        <pre>{ JSON.stringify(formulario)}</pre>
+
+    </form>
+  )
+}
